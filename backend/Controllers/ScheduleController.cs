9cb7002eac61b9debceec8c677634f81a318c71d
James Clarke <james.clarke@tcu.edu>
2024-10-27 20:28:10 -0500

    Endpoint controller mapping set up

--- backend/Controllers/ScheduleController.cs
+++ backend/Controllers/ScheduleController.cs
@@ -2,12 +2,15 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace backend.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class ScheduleController : Controller
     {
         // GET: ScheduleController
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            return Ok("Hello from schedule!");
         }
 
     }
