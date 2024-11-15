ad519695f4a32a603300e6369912a983a7dc2a66
katebednarz <112124950+katebednarz@users.noreply.github.com>
2024-11-15 10:55:50 -0600

    POST /gameSchedule test done

--- TestFrogCrew/Controllers/ScheduleControllerTests.cs
+++ TestFrogCrew/Controllers/ScheduleControllerTests.cs
@@ -36,9 +36,47 @@ namespace backend.Controllers.Tests
         }
 
         [Test()]
-        public void CreateGameScheduleTestSuccess()
+        public async Task CreateGameScheduleTestSuccess()
         {
-            Assert.Pass();
+            // Arrange
+            var request = new GameScheduleDTO
+            {
+                Sport = "Women's Basketball",
+                Season = "2024"
+            };
+
+            var mockSchedule = new Schedule
+            {
+                Id = 1,
+                Sport = request.Sport,
+                Season = request.Season
+            };
+
+            _mockContext?.Setup(c => c.Add(It.IsAny<Schedule>())).Callback<Schedule>(schedule => schedule.Id = mockSchedule.Id);
+            _mockContext?.Setup(c => c.SaveChanges()).Returns(1);
+
+            // Act
+            var result = await _controller!.CreateGameSchedule(request) as ObjectResult;
+            var response = result?.Value as Result;
+
+            // Assert
+            Assert.Multiple(() => {
+                Assert.IsNotNull(result);
+                Assert.IsTrue(response?.Flag); // Verify Flag
+                Assert.That(response?.Code, Is.EqualTo(200)); // Verify Code
+                Assert.That(response?.Message, Is.EqualTo("Add Success")); // Verify Message
+            });
+
+            // Verify Data
+            var gameScheduleDTO = response?.Data as GameScheduleDTO;
+            Assert.Multiple(() => {
+                Assert.IsNotNull(gameScheduleDTO);
+                Assert.That(gameScheduleDTO?.Sport, Is.EqualTo(request.Sport));
+                Assert.That(gameScheduleDTO?.Season, Is.EqualTo(request.Season));
+            });
+            // Verify Database Saves
+            _mockContext?.Verify(c => c.Add(It.IsAny<Schedule>()), Times.Once);
+            _mockContext?.Verify(c => c.SaveChanges(), Times.Exactly(1)); // 1 for schedule
         }
 
         [Test()]
