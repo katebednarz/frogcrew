0b382dce3563838dc0160caed683bd05db828ee3
JamesEdmonson0 <112120550+JamesEdmonson0@users.noreply.github.com>
2024-12-11 17:41:56 -0600

    c

---
+++ backend/Models/AuthDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Models;
+
+public class AuthDbContext : IdentityDbContext<ApplicationUser>
+{
+    public AuthDbContext(DbContextOptions<AuthDbContext> options)
+        : base(options)
+    {
+    }
+}
+
