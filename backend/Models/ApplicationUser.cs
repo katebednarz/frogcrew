603f3d6102bf22c33807ea0c034f8363f3a78dfe
James Edmonson <james.edmonson@tcu.edu>
2024-12-11 18:15:49 -0600

    Commit

--- backend/Models/ApplicationUser.cs
+++ backend/Models/ApplicationUser.cs
@@ -1,8 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿// using Microsoft.AspNetCore.Identity;
 
-namespace backend.Models;
+// namespace backend.Models;
 
-public class ApplicationUser : IdentityUser
-{
-}
+// public class ApplicationUser : IdentityUser
+// {
+// }
 
