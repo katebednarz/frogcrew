250120ddf74974f430847ac0b485aa6bd3d1a4b6
James Clarke <james.clarke@tcu.edu>
2025-02-17 09:50:33 -0600

    GET implementation

--- backend/Controllers/AvailabilityController.cs
+++ backend/Controllers/AvailabilityController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using backend.Models;
 using backend.DTO;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace backend.Controllers
@@ -59,6 +60,42 @@ namespace backend.Controllers
 
 			var response = new Result(true, 200, "Add Success", availabilityDTO);
 			return Ok(response);
+		}
+		
+		/*
+			* Adds availability for a user
+			* 
+			* @param request The availability to add
+			* @return The result of the operation
+		*/
+		[HttpGet("availability/{userId}")]
+		public async Task<IActionResult> GetAvailability(int userId)
+		{
+			if (userId == 0)
+			{
+				var errorResponse = new Result(false, 400, "User ID is required.", null);
+				return new ObjectResult(errorResponse) { StatusCode = 400 };
+			}
+			
+			var availabilityList = await _context.Availabilities
+				.Where(a => a.UserId == userId)
+				.Select(a => new AvailabilityDTO
+				{
+					UserId = a.UserId,
+					GameId = a.GameId,
+					Available = a.Available == 1,
+					Comments = a.Comments
+				})
+				.ToListAsync();
+
+			if (availabilityList.Count == 0)
+			{
+				var notFoundResponse = new Result(false, 404, $"Could not find user with id {userId}", null);
+				return new ObjectResult(notFoundResponse) { StatusCode = 404 };
+			}
+
+			var successResponse = new Result(true, 200, "Find Success", availabilityList);
+			return Ok(successResponse);
 		}
 	}
 }
