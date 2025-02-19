ad413e6ff81b30d3511b53f7214570db7d24be81
James Edmonson <james.edmonson@tcu.edu>
2025-02-19 13:49:22 -0600

    Export still in progress

--- backend/Controllers/CrewListController.cs
+++ backend/Controllers/CrewListController.cs
@@ -1,5 +1,7 @@
+using System.Text;
 using backend.DTO;
 using backend.Models;
+using backend.Utils;
 using Microsoft.AspNetCore.Mvc;
 
 namespace backend.Controllers
@@ -10,10 +12,12 @@ namespace backend.Controllers
     {
         
         private readonly FrogcrewContext _context;
+        private readonly DtoConverters _converters;
 
         public CrewListController(FrogcrewContext context)
         {
         _context = context;
+        _converters = new DtoConverters(_context);
         }
 
         /*
@@ -29,7 +33,30 @@ namespace backend.Controllers
                 return new ObjectResult(new Result(false, 404, $"Game with ID {gameId} not found.")) { StatusCode = 404 };
             }
 
-            return Ok(new Result(true, 200, "Find Success", game.ConvertToCrewListDTO(_context)));
+            return Ok(new Result(true, 200, "Find Success", _converters.GameToCrewListDto(game)));
+        }
+
+        [HttpGet("crewList/export/{gameId}")]
+        public async Task<IActionResult> ExportCrewList(int gameId)
+        {
+            var game = await _context.Games.FindAsync(gameId);
+            if (game == null) {
+                return new ObjectResult(new Result(false, 404, $"Game with ID {gameId} not found.")) { StatusCode = 404 };
+            }
+            
+            var crewList = _converters.GameToCrewListDto(game);
+
+            var csv = new StringBuilder();
+            csv.AppendLine("Position,Crew Member,ArrivalTime,Location");
+
+            foreach (var crew in crewList.CrewedMembers)
+            {
+                csv.AppendLine($"{crew.Position},{crew.UserId}");
+            }
+            
+            var bytes = Encoding.UTF8.GetBytes(csv.ToString());
+            return Ok(new Result(true, 200, "Export Success", csv.ToString()));
+            
         }
     }
 }
